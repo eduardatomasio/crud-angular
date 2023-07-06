67264b9c25c2e374db842db42b82674ea2782ff2
Duda Tomasio <dudasoarestomasio@gmail.com>
2023-07-06 20:48:44 -0300

    adds navigation button

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
   styleUrls: ['./product-create.component.css']
 })
-export class ProductCreateComponent {
+export class ProductCreateComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
 
 }
